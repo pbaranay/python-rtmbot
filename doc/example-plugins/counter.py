da0c81f7016756bebdbe20133a9ee95cc1fd66d0
Paul Baranay <paul@getwiser.com>
2016-03-01 14:44:01 -0500

    Cast time.time() as unicode

--- doc/example-plugins/counter.py
+++ doc/example-plugins/counter.py
@@ -6,4 +6,4 @@ crontable.append([5,"say_time"])
 
 def say_time():
     #NOTE: you must add a real channel ID for this to work
-    outputs.append(["D12345678", time.time()])
+    outputs.append(["D12345678", unicode(time.time())])
